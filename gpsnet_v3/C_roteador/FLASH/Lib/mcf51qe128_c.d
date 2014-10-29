1fb3cfdb6c12489073437953c1bef36f704286e0
barriquello <barriquello@gmail.com>
2014-10-29 12:11:04 -0200

    deleted

--- gpsnet_v3/C_roteador/FLASH/Lib/mcf51qe128_c.d
+++
@@ -1,5 +0,0 @@
-Lib/mcf51qe128_c.obj: C:\Users\Universidade\ Federal\workspace\gpsnet_v3\C_roteador\Lib\mcf51qe128.c \
-	C:\Freescale\CW\ MCU\ v10.4\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
-	C:\Freescale\CW\ MCU\ v10.4\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
-	C:\Freescale\CW\ MCU\ v10.4\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
-	C:\Freescale\CW\ MCU\ v10.4\MCU\ColdFire_Support\derivatives\device\include\mcf51qe128.h 
--- gpsnet_v3/C_roteador/FLASH/Sources/HARDWARE/startcf_c.d
+++
@@ -1,6 +0,0 @@
-Sources/HARDWARE/startcf_c.obj: C:\Users\Universidade\ Federal\workspace\gpsnet_v3\C_roteador\Sources\HARDWARE\startcf.c \
-	C:\Freescale\CW\ MCU\ v10.4\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
-	C:\Freescale\CW\ MCU\ v10.4\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
-	C:\Freescale\CW\ MCU\ v10.4\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
-	C:\Users\Universidade\ Federal\workspace\gpsnet_v3\C_roteador\Sources\HARDWARE\startcf.h \
-	C:\Freescale\CW\ MCU\ v10.4\MCU\ColdFire_Support\ewl\EWL_Runtime\Runtime_ColdFire\Source\RuntimeConfig.h 
